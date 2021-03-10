3b9cbb7707ef55ec925d25201e803928649d9866
Josh <joshvelasqez6@gmail.com>
2021-03-09 22:13:23 -0700

    Initial commit. Have basic functionality and alarm file.

---
+++ Windows/PomodoroTimer/PomodoroTimer/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace PomodoroTimer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int time = 20;
+        private DispatcherTimer Timer;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Timer = new DispatcherTimer();
+            Timer.Interval = new TimeSpan(0, 0, 1);
+            Timer.Tick += TimerTick;
+            Timer.Start();
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            if (time > 0)
+            {
+                time--;
+                TimerTextBlock.Text = string.Format("00:0{0}:0{1}", time / 60, time % 60);
+            } else
+            {
+                Timer.Stop();
+            }
+        }
+    }
+}

89ebec3518ca6b9d215a049e8da98f6d5a95d702
Josh <joshvelasqez6@gmail.com>
2021-03-15 22:54:48 -0600

    Added stop feature and continuous usage

--- Windows/PomodoroTimer/PomodoroTimer/MainWindow.xaml.cs
+++ Windows/PomodoroTimer/PomodoroTimer/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Media;
 using System.Windows;
 using System.Windows.Threading;
@@ -13,10 +14,11 @@ namespace PomodoroTimer
         private int time = 0;
         private DispatcherTimer Timer;
         private SoundPlayer player = null;
+
         public MainWindow()
         {
             InitializeComponent();
-
+            Topmost = true;
             Timer = new DispatcherTimer(DispatcherPriority.Send);
             Timer.Interval = new TimeSpan(0, 0, 1);
             Timer.Tick += TimerTick;
@@ -37,9 +39,12 @@ namespace PomodoroTimer
                 }
                 Activate();
                 Timer.Stop();
-                // POPUP THAT SAYS DONE!... BUTTON THAT STOPS PLAYING THE SOUND
                 player = new SoundPlayer(Properties.Resources.alarm);
                 player.PlayLooping();
+                Success success = new Success(player);
+                success.Show();
+                // Listen to when this invoked... once invoked then close then update the time!
+                success.NewTime += value => time = value;
             }
         }
 
@@ -71,24 +76,26 @@ namespace PomodoroTimer
 
         private void OnStartClick(object sender, RoutedEventArgs e)
         {
-            Timer.Stop();
-            Timer.Start();
+            if (time != 0)
+            {
+                StartButton.IsEnabled = false;
+                Timer.Start();
+            }
         }
 
         private void OnPauseClick(object sender, RoutedEventArgs e)
         {
-            Timer.Stop();
-            if (player != null)
+            if (time > 0)
             {
-                player.Stop();
+                Timer.Stop();
+                StartButton.IsEnabled = true;
             }
-            
         }
 
         private void OnCustomClick(object sender, RoutedEventArgs e)
         {
             Timer.Stop();
-            time = 15;
+            time = 5;
             UpdateTime();
         }
     }
--- Windows/PomodoroTimer/PomodoroTimer/Success.xaml.cs
+++ Windows/PomodoroTimer/PomodoroTimer/Success.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Media;
+using System.Windows;
 
 namespace PomodoroTimer
 {
@@ -7,9 +9,66 @@ namespace PomodoroTimer
     /// </summary>
     public partial class Success : Window
     {
-        public Success()
+        private SoundPlayer player = null;
+
+        public event Action<int> NewTime;
+
+        public Success(SoundPlayer player)
         {
             InitializeComponent();
+            Topmost = true;
+            this.player = player;
+        }
+
+        private void OnTakeABreakClick(object sender, RoutedEventArgs e)
+        {
+            if (NewTime != null)
+            {
+                NewTime(300);
+                CloseWindow();
+            }
+        }
+
+        private void OnMoreMinutesClick(object sender, RoutedEventArgs e)
+        {
+            if (NewTime != null)
+            {
+                NewTime(300);
+                CloseWindow();
+            }
+        }
+
+        private void OnStopAlarmClick(object sender, RoutedEventArgs e)
+        {
+            StopPlayer();
+        }
+
+        private void OnTakeALongBreakClick(object sender, RoutedEventArgs e)
+        {
+            if (NewTime != null)
+            {
+                NewTime(1800);
+                CloseWindow();
+            }
+        }
+
+        private void CloseWindow()
+        {
+            StopPlayer();
+            //Environment.Exit(0);
+        }
+
+        private void OnCloseWindow(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            CloseWindow();
+        }
+
+        private void StopPlayer()
+        {
+            if (player != null)
+            {
+                player.Stop();
+            }
         }
     }
 }
